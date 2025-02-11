d4e53c0f1f951b5de83909c0ac8c64bcf31fab26
sijalica <igorradivojevic996@gmail.com>
2025-02-11 18:51:59 +0100

    initial commit

---
+++ src/main/java/com/jobposts/demo/JobpostsApplication.java
@@ -0,0 +1,13 @@
+package com.jobposts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobposts/demo/JobpostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobposts.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
